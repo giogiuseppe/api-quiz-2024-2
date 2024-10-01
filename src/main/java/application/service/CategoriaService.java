142b32970e58caff1cce56b6a1c37195964f32a3
Giovanni Giuseppe <giovannirosa251500@gmail.com>
2024-10-01 17:51:44 +0000

    first commit

--- src/main/java/application/service/CategoriaService.java
+++ src/main/java/application/service/CategoriaService.java
@@ -1,8 +1,25 @@
 package application.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import application.model.Categoria;
+import application.record.CategoriaDTO;
+import application.repository.CategoriaRepository;
 
 @Service
 public class CategoriaService {
-    
+
+    @Autowired
+    private CategoriaRepository caterogiaRepo;
+
+    public Iterable<CategoriaDTO> findAll() {
+        return caterogiaRepo.findAll();
+    }
+
+    public CategoriaDTO insert(CategoriaDTO categoria) {
+        Categoria nova = caterogiaRepo.save(new Categoria(categoria));
+        return new CategoriaDTO(nova);
+    }
+
 }
